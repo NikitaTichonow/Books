149948d4979c0266af6e402cbf0f3059f5b3451e
Nikita <niktos@icloud.com>
2024-04-29 18:35:53 +0300

    rebase views commit

--- WorldBooks/catalog/views.py
+++ WorldBooks/catalog/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Главная страница сайта Мир книг!")
+    return HttpResponse("Главная страница сайта Мир книг!!!!!!!")
 
 
